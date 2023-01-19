3b6f9663752461137c7cf4e4396203a2c29a9f93
Oleksandr <dacenko@gmail.com>
2023-01-19 20:29:18 +0200

    Updated m_class.py mobule

--- m_class.py
+++ m_class.py
@@ -5,20 +5,30 @@ file_name = "Metaclass_and_fields.txt"
 
 class MetaClassUse(type):
 
-    def __new__(typeclass, classname, supers, classdict):
+    @classmethod
+    def write_class(mcs, classname, classdict):
 
-        with open(file_name, "r+") as f:
-            if classname not in f:
-                f.write(f"Class name = {classname}, ")
-                f.write(str([key for key in classdict]))
-                f.write("\n")
+        with open(file_name, "a") as f:
+            f.write(f"Class name = {classname}, ")
+            f.write(f"List of fields = {[key for key in classdict]}")
+            f.write("\n")
 
-        return type.__new__(typeclass, classname, supers, classdict)
+    def __new__(mcs, classname, supers, classdict):
+
+        MetaClassUse.write_class(classname, classdict)
+
+        return type.__new__(mcs, classname, supers, classdict)
 
 
 class Person(metaclass=MetaClassUse):
+    """
+    This is a class to represent a person.
+
+
+    """
 
     def __init__(self, surname, name, id_number):
+
 
         self.surname = surname
         self.name = name
@@ -27,6 +37,3 @@ class Person(metaclass=MetaClassUse):
     def __str__(self):
 
         return f"{self.surname} {self.name}"
-
-
-
--- main.py
+++ main.py
@@ -2,11 +2,6 @@ import m_class as ms
 
 if __name__ == "__main__":
 
-    class A(metaclass=ms.MetaClassUse):
-        pass
-
     person_1 = ms.Person("Petrenko", "Petro", 112233)
 
-    print(person_1)
-    # print(ms.Person.__dict__.keys())
-    # print(ms.MetaClassUse.__dict__.keys())
+
