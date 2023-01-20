7bee6253bd210fe89dd4a5740a2192ab1cff550d
Oleksandr <dacenko@gmail.com>
2023-01-20 12:56:00 +0200

    Added doc strings

--- m_class.py
+++ m_class.py
@@ -1,12 +1,19 @@
-
+"""This module provide access to MetaClassUse and Person classes."""
 
 file_name = "Metaclass_and_fields.txt"
 
 
 class MetaClassUse(type):
+    """This is the metaclass used to write name of class used this metaclass and its fields in txt file."""
 
-    @classmethod
-    def write_class(mcs, classname, classdict):
+    @staticmethod
+    def write_class(classname, classdict):
+        """
+        Static method used to write name of class used this metaclass and its fields in txt file.
+
+        :param classname: Name of the class used this metaclass.
+        :param classdict: Dictionary of fields of the class used this metaclass.
+        """
 
         with open(file_name, "a") as f:
             f.write(f"Class name = {classname}, ")
@@ -16,24 +23,18 @@ class MetaClassUse(type):
     def __new__(mcs, classname, supers, classdict):
 
         MetaClassUse.write_class(classname, classdict)
-
         return type.__new__(mcs, classname, supers, classdict)
 
 
 class Person(metaclass=MetaClassUse):
-    """
-    This is a class to represent a person.
-
-
-    """
+    """This is a class to represent a person."""
 
     def __init__(self, surname, name, id_number):
-
+        """Initialisation of the attributes of the class Person."""
 
         self.surname = surname
         self.name = name
         self.id_number = id_number
 
     def __str__(self):
-
         return f"{self.surname} {self.name}"
--- main.py
+++ main.py
@@ -3,5 +3,3 @@ import m_class as ms
 if __name__ == "__main__":
 
     person_1 = ms.Person("Petrenko", "Petro", 112233)
-
-
